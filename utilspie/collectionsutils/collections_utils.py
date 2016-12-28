2bccf9466e43fa91b8bf8dd7343d9647010741a9
Moin <moin18@gmail.com>
2016-12-28 22:00:59 +0530

    frozendict with __repr__ and changed exception to AttributeError

--- utilspie/collectionsutils/collections_utils.py
+++ utilspie/collectionsutils/collections_utils.py
@@ -1,7 +1,6 @@
-from ..exceptions import MethodCallNotAllowed
 
 
-class FrozenDict(dict):
+class frozendict(dict):
     def __key(self):
         return tuple((k,self[k]) for k in sorted(self))
 
@@ -11,39 +10,41 @@ class FrozenDict(dict):
     def __eq__(self, other):
         return self.__key() == other.__key()
 
+    def __repr__(self):
+        return '{}({})'.format(type(self).__name__, super(type(self), self).__repr__())
+
     def update(self, *args, **kwargs):
-        raise MethodCallNotAllowed("You can not call '{}()' for '{}' object".format(
+        raise AttributeError("You can not call '{}()' for '{}' object".format(
             self.update.__name__, type(self).__name__))
 
     def pop(self, *args, **kwargs):
-        raise MethodCallNotAllowed("You can not call '{}()' for '{}' object".format(
+        raise AttributeError("You can not call '{}()' for '{}' object".format(
             self.pop.__name__, type(self).__name__))
 
     def popitem(self, *args, **kwargs):
-        raise MethodCallNotAllowed("You can not call '{}()' for '{}' object".format(
+        raise AttributeError("You can not call '{}()' for '{}' object".format(
             self.popitem.__name__, type(self).__name__))
 
     def setdefault(self, *args, **kwargs):
-        raise MethodCallNotAllowed("You can not call '{}()' for '{}' object".format(
+        raise AttributeError("You can not call '{}()' for '{}' object".format(
             self.setdefault.__name__, type(self).__name__))
 
     def clear(self, *args, **kwargs):
-        raise MethodCallNotAllowed("You can not call '{}()' for '{}' object".format(
+        raise AttributeError("You can not call '{}()' for '{}' object".format(
             self.clear.__name__, type(self).__name__))
 
     def __setitem__(self, *args, **kwargs):
-        raise MethodCallNotAllowed("You can not call '{}()' for '{}' object".format(
+        raise AttributeError("You can not call '{}()' for '{}' object".format(
             self.__setitem__.__name__, type(self).__name__))
 
     def __setattr__(self, *args, **kwargs):
-        raise MethodCallNotAllowed("You can not call '{}()' for '{}' object".format(
+        raise AttributeError("You can not call '{}()' for '{}' object".format(
             self.__setattr__.__name__, type(self).__name__))
 
     def __delitem__(self, *args, **kwargs):
-        raise MethodCallNotAllowed("You can not call '{}()' for '{}' object".format(
+        raise AttributeError("You can not call '{}()' for '{}' object".format(
             self.__delitem__.__name__, type(self).__name__))
 
     def __delattr__(self, *args, **kwargs):
-        raise MethodCallNotAllowed("You can not call '{}()' for '{}' object".format(
+        raise AttributeError("You can not call '{}()' for '{}' object".format(
             self.__delattr__.__name__, type(self).__name__))
-
